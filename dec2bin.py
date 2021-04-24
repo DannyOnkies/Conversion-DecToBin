4f35b1130e1beb292f77fcd2359498edc6d47b8c
Danny <80686975+DannyOnkies@users.noreply.github.com>
2021-04-24 18:23:24 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -1,7 +1,5 @@
 ##
-# CONVERTE UN DECIMALE IN UN BINARIO
-# OTTIMIZZATO PER CIFRE SINO A 255
-#
+# CONVERTS A DECIMAL TO A BINARY 
 
 def dec_bin(n):
     b = ''
@@ -10,15 +8,8 @@ def dec_bin(n):
             b = '0' + b
         else:
             b = '1' + b
-        n = int(n / 2)  # casting in intero
-    # formatto il numero binario in 8 cifre;
-    # se sono di meno le inserisco davanti
-    if len(b) < 8:
-        # inserisco tanti zeri quanti
-        # ne mancano per arrivare a 8
-        for i in range(8 - len(b)):
-            b = "0" + b
+        n = int(n / 2)  # casting
     print(b)
 
-c = int(input("Inserisci un numero decimale: "))
+c = int(input("Enter a decimal number: "))
 dec_bin(c)
