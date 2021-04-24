e7dc2bb6b71e8e177e9f812332fb5df33ab58f4d
Danny <80686975+DannyOnkies@users.noreply.github.com>
2021-04-24 18:16:06 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -1,17 +1,30 @@
 ##
-# converts a decimal number to binary
+# CONVERTE UN DECIMALE IN UN BINARIO
+# OTTIMIZZATO PER CIFRE SINO A 255
 #
 
-def bin2dec(binario):
-    lung = len(binario)
-    dec = 0
-    for i in range(lung):
-        indice = lung - i - 1
-        cifra = int(binario[indice]) * pow(2, i)
-        dec = dec + cifra
-    return dec
+def dec_bin(n):
+    b = ''
+    while n > 0:
+        if n % 2 == 0:
+            b = '0' + b
+        else:
+            b = '1' + b
+        n = int(n / 2)  # casting in intero
+    # formatto il numero binario in 8 cifre;
+    # se sono di meno le inserisco davanti
+    if len(b) < 8:
+        # inserisco tanti zeri quanti
+        # ne mancano per arrivare a 8
+        for i in range(8 - len(b)):
+            b = "0" + b
+    # inserisco uno spazio tra i due gruppi di 4 cifre
+    if len(b) == 8:
+        val_form = b[:4] + ' ' + b[4:]
+    else:
+        val_form = b
+    print(val_form)
 
 
-bin2conv = input("Enter the binary number to convert to decimal ...")
-
-print(bin2dec(bin2conv))
+c = int(input("Inserisci un numero decimale: "))
+dec_bin(c)
